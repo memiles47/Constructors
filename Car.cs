50fe443cd66109a65caaf4f80a8e2453566ee7f6
Michael E Miles <michael@miles-me.net>
2017-06-15 06:06:58 -0500

    Constructors used during inheritance and using :base in the constructor of the class inheriting. Comments to come

---
+++ Car.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Constructors
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine($"Car is being initialized. {registrationNumber}");
+        }
+    }
+}
